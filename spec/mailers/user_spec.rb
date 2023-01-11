88354e69a62536eb5f800a4b5bd584a725ed842d
maxthestranger <thestrangermax@gmail.com>
2023-01-11 16:51:52 +0300

    add mail test

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "welcome_email" do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.welcome_email(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Welcome to Ticket Poa")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@ticketpoa.com"])
+
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Welcome to ticketpoa.com")
+    end
 end
