3f6ee58d44ca287da41d7a5061db534e36b0535a
maxthestranger <thestrangermax@gmail.com>
2023-01-16 19:15:18 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create all event categories
+categories = [
+  {
+    name: "Music",
+    cover_image: "music.jpg",
+  },
+  {
+    name: "Sports",
+    cover_image: "sports.jpg",
+  },
+  {
+    name: "Theatre",
+    cover_image: "theatre.jpg",
+  },
+  {
+    name: "Comedy",
+    cover_image: "comedy.jpg",
+  },
+  {
+    name: "Film",
+    cover_image: "film.jpg",
+  },
+  {
+    name: "Exhibitions",
+    cover_image: "exhibitions.jpg",
+  },
+  {
+    name: "Conferences",
+    cover_image: "conferences.jpg",
+  },
+  {
+    name: "Festivals",
+    cover_image: "festivals.jpg",
+  },
+  {
+    name: "Family",
+    cover_image: "family.jpg",
+  },
+  {
+    name: "Food & Drink",
+    cover_image: "food-and-drink.jpg",
+  },
+  {
+    name: "Charity",
+    cover_image: "charity.jpg",
+  },
+  {
+    name: "Religious",
+    cover_image: "religious.jpg",
+  },
+  {
+    name: "Educational",
+    cover_image: "educational.jpg",
+  },
+  {
+    name: "Health & Well-being",
+    cover_image: "health-and-well-being.jpg",
+  },
+  {
+    name: "Other",
+    cover_image: "other.jpg",
+  },
+]
+
+categories.each do |category|
+  Category.create(category)
+end
